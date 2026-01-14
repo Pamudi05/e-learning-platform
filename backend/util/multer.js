3f657351a53bbbb93d8ff4e265e7206c95ba0e25
Pamudi05 <113631350+Pamudi05@users.noreply.github.com>
2026-01-15 01:26:01 +0530

    changes

--- backend/util/multer.js
+++ backend/util/multer.js
@@ -1,6 +1,13 @@
 import multer from 'multer';
 import path from 'path';
 import { v4 as uuidv4 }from 'uuid';
+import fs from "fs";
+
+const uploadDir = "uploads";
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
