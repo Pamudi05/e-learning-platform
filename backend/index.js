310584f1e3e8607a7ad972c423c119f7666ff3e8
Pamudi05 <113631350+Pamudi05@users.noreply.github.com>
2026-01-14 17:06:46 +0530

    Fix

--- backend/index.js
+++ backend/index.js
@@ -41,7 +41,7 @@ const __dirname = path.dirname(__filename);
 const buildPath = path.join(__dirname, "../frontend/build");
 app.use(express.static(buildPath));
 
-app.get('*', (req, res) => {
+app.get('/:anything(.*)', (req, res) => {
   res.sendFile(path.join(buildPath, 'index.html'), (err) => {
     if (err) res.status(500).send(err);
   });
