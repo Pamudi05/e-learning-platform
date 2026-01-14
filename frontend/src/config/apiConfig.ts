f1b899d6b2fdb6323b7f8ed2a6c48ee02bb6ee35
Pamudi05 <113631350+Pamudi05@users.noreply.github.com>
2026-01-14 21:04:39 +0530

    changes

--- frontend/src/config/apiConfig.ts
+++ frontend/src/config/apiConfig.ts
@@ -1,3 +1,4 @@
-const BASE_URL = 'http://localhost:5000/api/v1';
+// const BASE_URL = 'http://localhost:5000/api/v1';
+const BASE_URL = 'https://your-backend.herokuapp.com';
 
 export default BASE_URL;
