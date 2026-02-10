dfebc4758c42c0ec10a2f6d8271cee1c94a94971
Pamudi05 <113631350+Pamudi05@users.noreply.github.com>
2026-02-10 09:37:50 +0530

    heroku updates

--- frontend/src/config/apiConfig.ts
+++ frontend/src/config/apiConfig.ts
@@ -1,3 +1,3 @@
-const BASE_URL = 'https://elearningapp-001-215c04782298.herokuapp.com/api/v1';
+const BASE_URL = 'https://elearningapp-6f8db1429e3d.herokuapp.com/api/v1';
 
 export default BASE_URL;
